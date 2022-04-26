4452d5b9b8bb34474f49d325501ed9a72ae96994
akira-cn <19511344@qq.com>
2022-04-26 16:07:30 +0800

    add css-doodle-collection

---
+++ docs/collections/css-doodle.jcoderc.js
@@ -0,0 +1,19 @@
+export default {
+  name: 'css-doodle ',
+  url: 'https://css-doodle.com/',
+  collection: [
+    {name: 'quick start', author: 'akira-cn', id: '7088238218914562088', default: true},
+    {
+      type: 'folder',
+      folded: true,
+      name: 'benchmark',
+      collection: [
+        {name: 'birds fly', author: 'akira-cn', id: '7088268165032968223'},
+        {name: 'group sealed', author: 'akira-cn', id: '7088273623122706466'},
+        {name: 'cloud', author: 'akira-cn', id: '7088274222738505732'},
+        {name: 'spot x 50000', author: 'akira-cn', id: '7088274902167322631'},
+        {name: '3d meshes', author: 'akira-cn', id: '7088275519128469512'},
+      ],
+    },
+  ],
+};
