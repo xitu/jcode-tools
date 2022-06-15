ac13bfc6678ee7c6743cad0bc41d5ade3f9bb37e
akira-cn <19511344@qq.com>
2022-06-15 18:12:01 +0800

    v0.2.2

--- dist/jcode-tools.js
+++ dist/jcode-tools.js
@@ -1,1 +1,1 @@
-window.JCode=window.JCode||{},JCode.getCustomCode=async()=>{let e;do{if(e=document.querySelector("body>script:last-of-type"),e)return e.textContent;await new Promise(t=>setTimeout(t,50))}while(1)};
+window.JCode=window.JCode||{},JCode.getCustomCode=async()=>{let e;do{if(e=document.querySelector("body>script:last-of-type"),e&&/^text\/.*/.test(e.type))return e.textContent;await new Promise(t=>setTimeout(t,50))}while(1)};
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ JCode.getCustomCode = async () => {
   let el;
   do {
     el = document.querySelector('body>script:last-of-type');
-    if(el) return el.textContent;
+    if(el && /^text\/.*/.test(el.type)) return el.textContent;
     // eslint-disable-next-line no-await-in-loop
     await new Promise(resolve => setTimeout(resolve, 50));
   } while(1);
