cbab2ded4ef12db1cd440a5845a13ca022638d07
akira-cn <19511344@qq.com>
2022-06-16 16:00:27 +0800

    add logger

--- src/index.js
+++ src/index.js
@@ -9,3 +9,24 @@ JCode.getCustomCode = async () => {
     await new Promise(resolve => setTimeout(resolve, 50));
   } while(1);
 };
+
+JCode.logger = (container, host = console) => {
+  const el = document.createElement('div');
+  el.className = 'jcode-logger';
+  container.append(el);
+  const makeLogger = (type) => {
+    return (...args) => {
+      console[type](...args);
+      const msg = args.join(' ');
+      const log = document.createElement('pre');
+      log.className = `jcode-logger__${type}`;
+      log.textContent = msg;
+      el.appendChild(log);
+    };
+  };
+  return {
+    log: makeLogger('log'),
+    warn: makeLogger('warn'),
+    error: makeLogger('error'),
+  };
+};
