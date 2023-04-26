0cd28fd45d001cfc91ed79e4fd97cfecf6da08d9
akira-cn <19511344@qq.com>
2023-04-26 10:57:56 +0800

    kv-aircode

--- src/storage.js
+++ src/storage.js
@@ -1,28 +1,15 @@
-/* cyrb53 */
-function hash(str, seed = 13977641) {
-  let h1 = 0xdeadbeef ^ seed,
-    h2 = 0x41c6ce57 ^ seed;
-  for(let i = 0, ch; i < str.length; i++) {
-    ch = str.charCodeAt(i);
-    h1 = Math.imul(h1 ^ ch, 2654435761);
-    h2 = Math.imul(h2 ^ ch, 1597334677);
-  }
-  h1 = Math.imul(h1 ^ (h1 >>> 16), 2246822507) ^ Math.imul(h2 ^ (h2 >>> 13), 3266489909);
-  h2 = Math.imul(h2 ^ (h2 >>> 16), 2246822507) ^ Math.imul(h1 ^ (h1 >>> 13), 3266489909);
-  return 4294967296 * (2097151 & h2) + (h1 >>> 0);
-}
-
-const kvHost = 'https://kv.juejin.fun';
+const kvHost = 'https://55aus9vavw.hk.aircode.run/storage';
 export class Storage {
-  constructor(bucket = hash(location.href)) {
+  constructor(bucket = location.href) {
     this.bucket = bucket;
   }
 
   async get(key) {
-    const res = await fetch(`${kvHost}/doc/${key}`, {
+    const res = await fetch(`${kvHost}?key=${key}`, {
       method: 'GET',
       headers: {
-        'X-Project-Id': this.bucket,
+        'Content-Type': 'application/json',
+        'X-Bucket-Id': this.bucket,
       },
     });
     const ret = await res.json();
@@ -34,12 +21,13 @@ export class Storage {
   }
 
   async set(key, value) {
-    const res = await fetch(`${kvHost}/doc/append/${key}`, {
+    const res = await fetch(kvHost, {
       method: 'POST',
       headers: {
-        'X-Project-Id': this.bucket,
+        'Content-Type': 'application/json',
+        'X-Bucket-Id': this.bucket,
       },
-      body: JSON.stringify({value}),
+      body: JSON.stringify({key, value}),
     });
     const ret = await res.json();
     this.result = ret;
@@ -47,11 +35,13 @@ export class Storage {
   }
 
   async del(key) {
-    const res = await fetch(`${kvHost}/doc/${key}`, {
-      method: 'DELETE',
+    const res = await fetch(kvHost, {
+      method: 'POST',
       headers: {
-        'X-Project-Id': this.bucket,
+        'Content-Type': 'application/json',
+        'X-Bucket-Id': this.bucket,
       },
+      body: JSON.stringify({key}),
     });
     const ret = await res.json();
     this.result = ret;
