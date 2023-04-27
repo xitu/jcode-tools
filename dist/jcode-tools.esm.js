f6e02cfdfa1e4d95c3234c7976e741da2cca2398
akira-cn <19511344@qq.com>
2023-04-27 09:15:42 +0800

    update dist

--- dist/jcode-tools.esm.js
+++ dist/jcode-tools.esm.js
@@ -684,27 +684,17 @@ var logger = (container, host = _console) => {
 };
 
 // src/storage.js
-function hash(str, seed = 13977641) {
-  let h1 = 3735928559 ^ seed, h2 = 1103547991 ^ seed;
-  for (let i = 0, ch; i < str.length; i++) {
-    ch = str.charCodeAt(i);
-    h1 = Math.imul(h1 ^ ch, 2654435761);
-    h2 = Math.imul(h2 ^ ch, 1597334677);
-  }
-  h1 = Math.imul(h1 ^ h1 >>> 16, 2246822507) ^ Math.imul(h2 ^ h2 >>> 13, 3266489909);
-  h2 = Math.imul(h2 ^ h2 >>> 16, 2246822507) ^ Math.imul(h1 ^ h1 >>> 13, 3266489909);
-  return 4294967296 * (2097151 & h2) + (h1 >>> 0);
-}
-var kvHost = "https://kv.juejin.fun";
+var kvHost = "https://55aus9vavw.hk.aircode.run/storage";
 var Storage = class {
-  constructor(bucket = hash(location.href)) {
+  constructor(bucket = location.href) {
     this.bucket = bucket;
   }
   async get(key) {
-    const res = await fetch(`${kvHost}/doc/${key}`, {
+    const res = await fetch(`${kvHost}?key=${key}`, {
       method: "GET",
       headers: {
-        "X-Project-Id": this.bucket
+        "Content-Type": "application/json",
+        "X-Bucket-Id": this.bucket
       }
     });
     const ret = await res.json();
@@ -715,23 +705,26 @@ var Storage = class {
     return ret.result.value;
   }
   async set(key, value) {
-    const res = await fetch(`${kvHost}/doc/append/${key}`, {
+    const res = await fetch(kvHost, {
       method: "POST",
       headers: {
-        "X-Project-Id": this.bucket
+        "Content-Type": "application/json",
+        "X-Bucket-Id": this.bucket
       },
-      body: JSON.stringify({ value })
+      body: JSON.stringify({ key, value })
     });
     const ret = await res.json();
     this.result = ret;
     return !ret.error;
   }
   async del(key) {
-    const res = await fetch(`${kvHost}/doc/${key}`, {
-      method: "DELETE",
+    const res = await fetch(kvHost, {
+      method: "POST",
       headers: {
-        "X-Project-Id": this.bucket
-      }
+        "Content-Type": "application/json",
+        "X-Bucket-Id": this.bucket
+      },
+      body: JSON.stringify({ key })
     });
     const ret = await res.json();
     this.result = ret;
